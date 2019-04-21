9c4c8e6e521b4431126b157a424cd87566e85d8f
yuanyuily <496629564@qq.com>
2019-04-21 21:09:05 +0800

    本地工程

---
+++ git_test/src/com/itheima/test/git1.java
@@ -0,0 +1,11 @@
+package com.itheima.test;
+
+public class git1 {
+    public static void main(String[] args) {
+        System.out.println("这是一个测试git");
+        System.out.println("这是一个测试git");
+        System.out.println("这是一个测试git");
+        System.out.println("这是一个测试git");
+        System.out.println("这是一个测试git");
+    }
+}
